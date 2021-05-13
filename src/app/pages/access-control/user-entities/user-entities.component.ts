b292ae1cfa05f802e265ea675edd8e317ff6f5e8
Rajeev Pai <rajeevpai.code@gmail.com>
2021-05-13 09:54:54 +0530

    Add trailing comma

--- src/app/pages/access-control/user-entities/user-entities.component.ts
+++ src/app/pages/access-control/user-entities/user-entities.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './user-entities.component.html',
-  styleUrls: ['./user-entities.component.scss']
+  styleUrls: ['./user-entities.component.scss'],
 })
 export class QnbUserEntitiesComponent { }
