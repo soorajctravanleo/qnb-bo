be1b0cf7cfe26bd3265b445576c7f2ec9cf32fa9
Rajeev Pai <rajeevpai.code@gmail.com>
2021-05-13 18:52:13 +0530

    Integrate mock login

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { mergeMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+import { User } from '../_helpers/models/frontend';
+import { LoginResponse } from '../_helpers/types/backend/index';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QnbAuthService {
+  private readonly AUTH_KEY_KEY = 'ak';
+
+  currentUser?: User = null;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(username: string, password: string) {
+    return this.http
+      .post('/authenticate', {
+        username,
+        password,
+      })
+      .pipe(
+        mergeMap(data => {
+          const { id, username, token, firstName, lastName } = data as LoginResponse;
+          this.currentUser = new User(id, username, token, firstName, lastName);
+          sessionStorage.setItem(this.AUTH_KEY_KEY, token);
+          return of(this.currentUser);
+        }),
+      );
+  }
+
+  logout() {
+    sessionStorage.removeItem(this.AUTH_KEY_KEY);
+  }
+
+  getCurrentUser() {
+    return this.currentUser;
+  }
+}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { QnbAuthService } from '../auth.service';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class QnbLoginComponent { }
+export class QnbLoginComponent {
+
+  @ViewChild('loginForm') loginForm: NgForm;
+
+  loginInProgress = false;
+  hasError = false;
+  errorMessage = '';
+
+  constructor(
+    private qnbAuthService: QnbAuthService,
+    private router: Router,
+  ) { }
+
+  loginHandler() {
+    if (!this.loginInProgress) {
+      this.loginInProgress = true;
+      this.hasError = false;
+      const { username, password } = this.loginForm.value;
+  
+      this.qnbAuthService
+        .login(username, password)
+        .subscribe(
+          res => {
+            this.loginInProgress = false;
+            this.router.navigateByUrl('/pages');
+          },
+          err => {
+            this.loginInProgress = false;
+            this.hasError = true;
+            this.errorMessage = err.error.message;
+          },
+        );
+    }
+  }
+}
