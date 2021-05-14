63847449fc4b6c5c22d4f854704d5678bdac1c40
Mahesh EU <mahesheu@gmail.com>
2021-05-14 21:25:24 +0530

    removed user create component

--- src/app/pages/access-control/roles/roles.component.ts
+++ src/app/pages/access-control/roles/roles.component.ts
@@ -1,27 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  NbSortDirection,
-  NbSortRequest,
-  NbTreeGridDataSource,
-  NbTreeGridDataSourceBuilder,
-} from '@nebular/theme';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NbDialogService } from '@nebular/theme';
 import { CreateRoleComponent } from './create-role/create-role.component';
-interface TreeNode<T> {
-  data: T;
-  children?: TreeNode<T>[];
-  expanded?: boolean;
-}
-
-interface FSEntry {
-  role_name: string;
-  description: string;
-  status: string;
-  multi_login: string;
-  no_password_expiry: string;
-  maker_id: string;
-  maker_date: string;
-}
 
 @Component({
   selector: 'qnb-roles',
@@ -29,73 +8,29 @@ interface FSEntry {
   styleUrls: ['./roles.component.scss'],
 })
 export class QnbRolesComponent implements OnInit {
+  elements: any = [
+    { role_name: "Viewer", description: 'Viewer has only view access', multi_login: 'No', nok_login: 'Yes', decent_login: 'Yes', no_password_expiry: 'Yes', created_date: '13th May' },
+    { role_name: "Admin", description: 'Viewer has edit permissions', multi_login: 'No', nok_login: 'Yes', decent_login: 'Yes', no_password_expiry: 'Yes', created_date: 'No' },
+    { role_name: "System", description: 'system has all previlages', multi_login: 'No', nok_login: 'Yes', decent_login: 'Yes', no_password_expiry: 'Yes', created_date: 'No' },
 
-  customColumn = 'role_name';
-  defaultColumns = ['description', 'status', 'multi_login', 'no_password_expiry', 'maker_id', 'maker_date'];
-  allColumns = [this.customColumn, ...this.defaultColumns];
+  ];
 
-  dataSource: NbTreeGridDataSource<any>;
+  headElements = ['Role Name', 'Description', 'Multi Login', 'Nok Login', 'Decent Login', 'No Password Expiry', 'Created Date'];
 
-  sortColumn: string = '';
-  sortDirection: NbSortDirection = NbSortDirection.NONE;
 
-  constructor(private dataSourceBuilder: NbTreeGridDataSourceBuilder<any>, private dialogService: NbDialogService) {
-    this.dataSource = this.dataSourceBuilder.create(this.data);
+  constructor(private dialogService: NbDialogService) {
+
   }
 
   ngOnInit(): void {
+
   }
 
-  changeSort(sortRequest: NbSortRequest): void {
-    this.dataSource.sort(sortRequest);
-    this.sortColumn = sortRequest.column;
-    this.sortDirection = sortRequest.direction;
-  }
 
-  getDirection(column: string): NbSortDirection {
-    if (column === this.sortColumn) {
-      return this.sortDirection;
-    }
-    return NbSortDirection.NONE;
-  }
-
-  private data: TreeNode<FSEntry>[] = [
-    {
-      data: {
-        role_name: 'system',
-        description: 'system has access to all menus and features',
-        status: 'ACTIVE',
-        multi_login: 'YES',
-        no_password_expiry: 'YES',
-        maker_id: 'Francis',
-        maker_date: '14th May 2022',
-      },
-    },
-    {
-      data: {
-        role_name: 'admin',
-        description: 'admin has limited access to all features',
-        status: 'ACTIVE',
-        multi_login: 'YES',
-        no_password_expiry: 'NO',
-        maker_id: 'Francis',
-        maker_date: '16th May 2022',
-      },
-    },
-    {
-      data: {
-        role_name: 'viewer',
-        description: 'viewer has access only readonly access',
-        status: 'ACTIVE',
-        multi_login: 'YES',
-        no_password_expiry: 'NO',
-        maker_id: 'Francis',
-        maker_date: '18th May 2022',
-      },
-    },
-  ];
   open() {
     const dialogRef = this.dialogService.open(CreateRoleComponent, {
     });
   }
+
+
 }
