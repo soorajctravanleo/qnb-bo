f2a81ae25db1805c5c02682e8ba184fa66742cec
Mahesh EU <mahesheu@gmail.com>
2021-05-14 09:32:54 +0530

    roles table

--- src/app/pages/access-control/roles/roles.module.ts
+++ src/app/pages/access-control/roles/roles.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from "@angular/common";
+import { NbTreeGridModule, NbToggleModule, NbDialogModule, NbDatepickerModule, NbSelectModule, NbButtonModule, NbCardModule, NbCheckboxModule, NbInputModule, NbRadioModule, NbTabsetModule } from '@nebular/theme';
 import { QnbRolesRoutingModule } from './roles-routing.module';
 import { QnbRolesComponent } from './roles.component';
 
@@ -8,7 +9,13 @@ import { QnbRolesComponent } from './roles.component';
     QnbRolesComponent,
   ],
   imports: [
+    CommonModule,
     QnbRolesRoutingModule,
+    NbTreeGridModule,
+    NbDialogModule,
+    NbSelectModule,
+    NbCardModule,
+    NbCheckboxModule,
   ],
 })
 export class QnbRolesModule { }
