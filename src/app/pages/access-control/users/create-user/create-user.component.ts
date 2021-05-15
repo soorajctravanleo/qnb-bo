c405828970202ecbb13be3a82caff43d3adf09eb
Mahesh EU <mahesheu@gmail.com>
2021-05-15 19:18:27 +0530

    validations

--- src/app/pages/access-control/users/create-user/create-user.component.ts
+++ src/app/pages/access-control/users/create-user/create-user.component.ts
@@ -71,22 +71,21 @@ export class CreateUserComponent implements OnInit {
 
     if (this.signupForm.valid) {
       this.userService.createUser(this.signupForm.value).subscribe(res => {
-
       });
 
       console.log(this.signupForm.value);
     } else {
-      this.validateAllFormFields(this.signupForm); //{7}
+      this.validateAllFormFields(this.signupForm);
     }
   }
 
-  validateAllFormFields(formGroup: FormGroup) {         //{1}
-    Object.keys(formGroup.controls).forEach(field => {  //{2}
-      const control = formGroup.get(field);             //{3}
-      if (control instanceof FormControl) {             //{4}
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
         control.markAsTouched({ onlySelf: true });
-      } else if (control instanceof FormGroup) {        //{5}
-        this.validateAllFormFields(control);            //{6}
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
       }
     });
 
