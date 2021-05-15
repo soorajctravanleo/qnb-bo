e77adbf5a0b69f84fea4e72f62058561c155a4b7
Rajeev Pai <rajeevpai.code@gmail.com>
2021-05-15 16:06:44 +0530

    Fix linting errors

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import {  
+import {
   GET_USERS,
   CREATE_USER,
   EDIT_USER,
