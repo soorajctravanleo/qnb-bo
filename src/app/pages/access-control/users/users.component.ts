4ac9bec4f77792ee5dbd9d473ce4ed452f27410e
Rajeev Pai <rajeevpai.code@gmail.com>
2021-05-16 13:00:47 +0530

    Fix linting errors

--- src/app/pages/access-control/users/users.component.ts
+++ src/app/pages/access-control/users/users.component.ts
@@ -5,4 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
-export class QnbUsersComponent{ }
+export class QnbUsersComponent { }
