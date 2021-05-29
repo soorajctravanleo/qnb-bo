3686735d1998cce0d4c39eb16642490743d8bf91
Mahesh EU <mahesheu@gmail.com>
2021-05-29 15:10:37 +0530

    merge resolved

--- src/app/pages/access-control/pending-requests/pending-requests.component.ts
+++ src/app/pages/access-control/pending-requests/pending-requests.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'qnb-pending-requests',
   templateUrl: './pending-requests.component.html',
-  styleUrls: ['./pending-requests.component.scss']
+  styleUrls: ['./pending-requests.component.scss'],
 })
 export class PendingRequestsComponent implements OnInit {
   headElements: any = ['Request No.', 'Type', 'Access Level', 'Role/userid', 'Entity', 'Application Type', 'Maker Id', 'Maker Date'];
