588085a48b504a199df058c85e2c6acf39f13031
Muznah Musthafa <muznahm94@gmail.com>
2021-05-30 17:40:19 +0530

    Finish Mock Unit

--- src/app/_helpers/models/backend/unit.ts
+++ src/app/_helpers/models/backend/unit.ts
@@ -1,3 +1,18 @@
+export class MockTransactionDesc {
+  constructor(
+    public id: number,
+    public modelId: number,
+    public name: string,
+  ) {}
+}
+
+export class MockUnitModule {
+  constructor(
+    public id: number,
+    public name: string,
+    public code: string,
+  ) {}
+}
 export class MockUnit {
   constructor(
     public code: string,
