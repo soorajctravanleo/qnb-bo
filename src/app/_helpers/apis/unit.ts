3d2f51071226168532ad1a823e241ef186603af4
Muznah Musthafa <muznahmm@Muznahs-MacBook-Air.local>
2021-05-31 17:02:27 +0530

    Finish Mock Unit ModuleName TransactionDescs

--- src/app/_helpers/apis/unit.ts
+++ src/app/_helpers/apis/unit.ts
@@ -1,1 +1,5 @@
 export const GET_UNITS = '/units';
+export const GET_ACCOUNT_TRANSACTION_DESCS= '/unit/accounts/transaction-desc';
+export const GET_CHEQUES_TRANSACTION_DESCS= '/unit/cheques/transaction-desc';
+export const GET_CONTACT_BANK_TRANSACTION_DESCS= '/unit/contact-bank/transaction-desc';
+export const GET_IPO_TRANSACTION_DESCS= '/unit/ipo/transaction-desc';
--- src/app/_helpers/models/backend/unit.ts
+++ src/app/_helpers/models/backend/unit.ts
@@ -1,18 +1,47 @@
-export class MockTransactionDesc {
+// export class MockTransactionDesc {
+//   constructor(
+//     public id: number,
+//     public modelId: number,
+//     public name: string,
+//   ) {}
+// }
+
+export class MockAccountsTransactionDesc {
   constructor(
     public id: number,
-    public modelId: number,
     public name: string,
   ) {}
 }
 
-export class MockUnitModule {
+export class MockChequesTransactionDesc {
   constructor(
     public id: number,
     public name: string,
-    public code: string,
   ) {}
 }
+
+export class MockContactBankTransactionDesc {
+  constructor(
+    public id: number,
+    public name: string,
+  ) {}
+}
+
+export class MockIPOTransactionDesc {
+  constructor(
+    public id: number,
+    public name: string,
+  ) {}
+}
+
+// export class MockUnitModule {
+//   constructor(
+//     public id: number,
+//     public name: string,
+//     public code: string,
+//   ) {}
+// }
+
 export class MockUnit {
   constructor(
     public code: string,
