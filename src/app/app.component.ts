a1892204779f78861a1b1f93c89265f3604e64a5
Rajeev Pai <rajeevpai.code@gmail.com>
2021-06-01 19:18:20 +0530

    Remove unwanted data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,3 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
 import { SeoService } from './@core/utils/seo.service';
@@ -13,8 +8,10 @@ import { SeoService } from './@core/utils/seo.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private analytics: AnalyticsService, private seoService: SeoService) {
-  }
+  constructor(
+    private analytics: AnalyticsService,
+    private seoService: SeoService,
+  ) { }
 
   ngOnInit(): void {
     this.analytics.trackPageViews();
