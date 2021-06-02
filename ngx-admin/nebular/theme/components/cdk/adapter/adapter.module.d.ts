860456417874e5aebc30f9f5d6dee6849d375e17
francis <jesusloves.francis@gmail.com>
2021-06-03 02:13:51 +0530

    seting up ngx-local

---
+++ ngx-admin/nebular/theme/components/cdk/adapter/adapter.module.d.ts
@@ -0,0 +1,10 @@
+import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
+export declare class NbCdkAdapterModule {
+    static forRoot(): ModuleWithProviders<NbCdkAdapterModule>;
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<NbCdkAdapterModule, never>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<NbCdkAdapterModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<NbCdkAdapterModule>;
+}
+
+//# sourceMappingURL=adapter.module.d.ts.map
---
+++ ngx-admin/nebular/theme/components/cdk/bidi/bidi.module.d.ts
@@ -0,0 +1,9 @@
+import { BidiModule } from '@angular/cdk/bidi';
+import * as ɵngcc0 from '@angular/core';
+export declare class NbBidiModule extends BidiModule {
+    static ɵfac: ɵngcc0.ɵɵFactoryDef<NbBidiModule, never>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<NbBidiModule, never, never, never>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<NbBidiModule>;
+}
+
+//# sourceMappingURL=bidi.module.d.ts.map
