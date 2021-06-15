21d6254af235a4a7678d2da504ba4e1a15e830ae
Muznah Musthafa <muznahmm@Muznahs-MacBook-Air.local>
2021-06-15 17:54:01 +0530

    Finish Mock Data for Beneficiary Popup

--- src/app/_helpers/models/backend/beneficiary-authorization.ts
+++ src/app/_helpers/models/backend/beneficiary-authorization.ts
@@ -8,5 +8,17 @@ export class MockBeneficiaryAuthorization {
     public lastViewedBy: string,
     public lastViewedDateOrTime: string,
     public channel: string,
+    public bankName: string,
+    public branch: string,
+    public city: string,
+    public beneficiaryName: string,
+    public beneficiaryAccountNo: string,
+    public currencyOfBeneficiaryAcc: string,
+    public intermediateBankSwiftAcc: string,
+    public swiftCode: string,
+    public routing: string,
+    public sortOrChips: number,
+    public mobileNo: number,
+    public customerNationalId: string
   ) {}
 }
