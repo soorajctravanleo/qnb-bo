90efc1ef890574d9ab7297819dd33371cdc200a2
hasifpcvlm <hasifpcvlm@gmail.com>
2021-06-16 12:16:19 +0530

    pending approval 80%

--- src/app/pages/access-control/pending-approvals/approval-details/approval-details.component.ts
+++ src/app/pages/access-control/pending-approvals/approval-details/approval-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NbDialogRef, NbToastrService } from '@nebular/theme';
 import { QnbPendingApprovalService } from '../../../../services';
 
 @Component({
@@ -7,23 +8,37 @@ import { QnbPendingApprovalService } from '../../../../services';
   styleUrls: ['./approval-details.component.scss'],
 })
 export class ApprovalDetailsComponent implements OnInit {
+  private index: number = 0;
   data: any;
-  constructor( private pendingapprovalservice: QnbPendingApprovalService, ) { }
+  comment: string;
+  constructor( private pendingapprovalservice: QnbPendingApprovalService,
+    private toastrService: NbToastrService,
+    protected ref: NbDialogRef<ApprovalDetailsComponent>,
+     ) { }
 
-  ngOnInit(): void {
-
+  ngOnInit(): void { console.log(this.data)
   }
 
-  approve() {
+  showToast(position, status, action) {
+    this.index += 1;
+    this.toastrService.show(
+      status || 'Success',
+      `User ${action}`,
+      { position, status });
+  }
+
+  approve(action: string) {
     const data = {
-      workflowId: this.data.requestId,
+      workflowId: this.data.workflowId,
+      checkerComments: this.comment,
+      workflowAction: action,
     };
     this.pendingapprovalservice.approveUser(data).subscribe(res => {
+      this.showToast('top-right', 'success', action);
+      this.ref.close({ refreshList: true });
     });
   }
 
   return() {}
-
-  decline() {}
 
 }
