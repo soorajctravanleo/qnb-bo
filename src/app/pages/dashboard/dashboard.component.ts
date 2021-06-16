7e84f09f9bd8894ed0faa769d78ebf5c46cf50a3
Mahesh EU <mahesheu@gmail.com>
2021-06-16 18:02:30 +0530

    minor changes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NbDialogService } from '@nebular/theme';
-import { QnbPendingApprovalService } from '../../services';
+import { QnbPendingApprovalService, QnbPendingRequestService } from '../../services';
 import { ApprovalDetailsComponent } from '../access-control/pending-approvals/approval-details/approval-details.component';
 
 @Component({
@@ -13,40 +13,47 @@ export class QnbDashboardComponent implements OnInit {
   roles = [];
   users = [];
 
+
   constructor(private pending_approval_service: QnbPendingApprovalService,
+    private pendingRequestService: QnbPendingRequestService,
     private dialogService: NbDialogService,
-  ) {}
+  ) { }
 
-ngOnInit(): void {
-  this.Getpendingworkflows();
-}
+  ngOnInit(): void {
+    this.Getpendingworkflows();
+    this.pendingRequestService.getPendingRequestUsers()
+      .subscribe(data => {
+        console.log(data);
+        this.users = data;
+      });
+  }
 
-Getpendingworkflows() {
-  this.userids = [];
-  this.pending_approval_service.getPendingWorkflows().subscribe((res: any) => {
-    res.map(t => {
-      this.userids.push(t.requestId);
+  Getpendingworkflows() {
+    this.userids = [];
+    this.pending_approval_service.getPendingWorkflows().subscribe((res: any) => {
+      res.map(t => {
+        this.userids.push(t.requestId);
+      });
+    }).add(() => {
+      const data = { requestIds: this.userids };
+      this.pending_approval_service.fetchPendingUsers(data).subscribe(res => {
+        this.users = res;
+      });
     });
-  }).add(() => {
-    const data = { requestIds: this.userids };
-    this.pending_approval_service.fetchPendingUsers(data).subscribe(res => {
-      this.users = res;
-    });
-  });
-}
+  }
 
-showDetails(data) {
-  this.dialogService
-    .open(ApprovalDetailsComponent, {
-      context: {
-        data: data,
-      },
-    })
-    .onClose.subscribe((event) => {
-      if (event?.refreshList) {
-        this.Getpendingworkflows();
-      }
-    });
-}
+  showDetails(data) {
+    this.dialogService
+      .open(ApprovalDetailsComponent, {
+        context: {
+          data: data,
+        },
+      })
+      .onClose.subscribe((event) => {
+        if (event?.refreshList) {
+          this.Getpendingworkflows();
+        }
+      });
+  }
 
 }
