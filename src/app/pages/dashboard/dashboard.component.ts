02d1fb7e29723b74fc390356a261cb0cd5b068c4
Mahesh EU <mahesheu@gmail.com>
2021-06-18 09:57:02 +0530

    dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NbDialogService } from '@nebular/theme';
 import { QnbPendingApprovalService, QnbPendingRequestService } from '../../services';
 import { ApprovalDetailsComponent } from '../access-control/pending-approvals/approval-details/approval-details.component';
-
+import { RequestDetailsComponent } from '../access-control/pending-requests/request-details/request-details.component'
 @Component({
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
@@ -20,18 +20,20 @@ export class QnbDashboardComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.Getpendingworkflows();
+    this.getpendingworkflows();
+    this.getPendingRequests();
+  }
+  getPendingRequests() {
     this.pendingRequestService.getPendingRequestUsers()
       .subscribe(data => {
         this.users = data;
       });
   }
-
-Getpendingworkflows() {
-  this.pending_approval_service.getPendingRequestUsers().subscribe((res: any) => {
-  this.users = res;
-  });
-}
+  getpendingworkflows() {
+    this.pending_approval_service.getPendingRequestUsers().subscribe((res: any) => {
+      this.users = res;
+    });
+  }
 
   showDetails(data) {
     this.dialogService
@@ -42,7 +44,20 @@ Getpendingworkflows() {
       })
       .onClose.subscribe((event) => {
         if (event?.refreshList) {
-          this.Getpendingworkflows();
+          this.getpendingworkflows();
+        }
+      });
+  }
+  showPendingDetails(data) {
+    this.dialogService
+      .open(RequestDetailsComponent, {
+        context: {
+          data: data,
+        },
+      })
+      .onClose.subscribe((event) => {
+        if (event?.refreshList) {
+          this.getPendingRequests();
         }
       });
   }
